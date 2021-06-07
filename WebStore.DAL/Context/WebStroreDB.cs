d68cb4d743c31dc5f95ff47be768f2f3af4df62e
KateM <molinakate888@gmail.com>
2021-06-06 21:10:59 -0500

    DALLib_Added

--- WebStore.DAL/Context/WebStroreDB.cs
+++ WebStore.DAL/Context/WebStroreDB.cs
@@ -1,10 +1,21 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore;
+using WebStore.Domain.Entities;
+
 namespace WebStore.DAL.Context
 {
-    public class WebStroreDB
+    public class WebStroreDB:DbContext
     {
-        public WebStroreDB()
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Brand> Brands { get; set; }
+
+        DbSet<Section> Sections { get; set; }
+
+        public WebStroreDB(DbContextOptions<WebStroreDB> options):base(options)
         {
+            //git 1.5
         }
     }
 }
+ 
--- WebStore.Domain/Entities/Brand.cs
+++ WebStore.Domain/Entities/Brand.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using WebStore.Domain.Entities.Base;
 using WebStore.Domain.Entities.Base.Interfaces;
 
@@ -7,5 +9,8 @@ namespace WebStore.Domain.Entities
     public class Brand : NamedEntity, IOrderedEntity
     {
         public int Order { get; set; }
+        
+        public ICollection<Product> Products { get; set; }
+
     }
 }
