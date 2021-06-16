65454776b648950486e69be28c4dd772006646d4
KateM <molinakate888@gmail.com>
2021-06-16 10:38:23 -0500

    CartServiceAndCartCoockiePrepared

---
+++ WebStore_MVC/Services/InCoockies/InCoockiesCartService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore_MVC.Services.InCoockies
+{
+    public class InCoockiesCartService
+    {
+    }
+}
