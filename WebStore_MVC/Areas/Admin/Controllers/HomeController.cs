a4a71781a4cdfafe6d52668f69b700cdabdb31f3
KateM <molinakate888@gmail.com>
2021-06-21 16:05:03 -0500

    AdminAreaDesign

--- WebStore_MVC/Areas/Admin/Controllers/HomeController.cs
+++ WebStore_MVC/Areas/Admin/Controllers/HomeController.cs
@@ -1,12 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebStore.Domain.Entities.Identity;
 
 namespace WebStore_MVC.Areas.Admin.Controllers
 {
     [Area("admin")]
+    [Authorize(Roles =Role.administrators)]
     public class HomeController : Controller
     {
         public IActionResult Index()
