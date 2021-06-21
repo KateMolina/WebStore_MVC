324aedba39d2ec4b3383691f37505d16c2c17cac
KateM <molinakate888@gmail.com>
2021-06-21 17:36:09 -0500

    OrderEntityDBSetApplied

---
+++ WebStore_MVC/Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Domain.Entities.Identity;
+using WebStore_MVC.Services.Interfaces;
+
+namespace WebStore_MVC.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    [Authorize(Roles = Role.administrators)]
+    public class ProductsController : Controller
+    {
+        private readonly IProductData productData;
+
+        public ProductsController(IProductData productData)
+        {
+            this.productData = productData;
+        }
+        public IActionResult Index()
+        {
+            return View(productData.GetProducts());
+        }
+
+        public IActionResult Edit(int id) =>
+            productData.GetProductById(id) is { } product ? View(product) : NotFound();
+
+        
+
+        public IActionResult Delete(int id) => 
+            productData.GetProductById(id) is { } product ? View(product) : NotFound();
+
+
+    }
+}
