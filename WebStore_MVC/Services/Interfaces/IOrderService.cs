321b7503c2a29420f64a1731c8632ee953f3e713
KateM <molinakate888@gmail.com>
2021-06-21 17:45:50 -0500

    OrderServiceModel

--- WebStore_MVC/Services/Interfaces/IOrderService.cs
+++ WebStore_MVC/Services/Interfaces/IOrderService.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebStore.Domain.Entities.Orders;
+using WebStore_MVC.ViewModels;
 
 namespace WebStore_MVC.Services.Interfaces
 {
     interface IOrderService
     {
+        Task<IEnumerable<Order>> GetUserOrder(string userName);
+
+        Task<Order> GetOrderById(int id);
+
+        Task<Order> CreateOrder(string userName, CartViewModel cart, OrderViewModel orderViewModel);
     }
 }
