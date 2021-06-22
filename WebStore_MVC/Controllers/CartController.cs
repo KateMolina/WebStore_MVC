d8227ea03ed5256fc0226510367de4a08c67d715
KateM <molinakate888@gmail.com>
2021-06-21 23:22:59 -0500

    UserProfileFunctionality_Added

--- WebStore_MVC/Controllers/CartController.cs
+++ WebStore_MVC/Controllers/CartController.cs
@@ -42,6 +42,7 @@ namespace WebStore_MVC.Controllers
             return RedirectToAction("Index", "Cart");
         }
         [Authorize]
+        [HttpPost, ValidateAntiForgeryToken]
         public async Task<IActionResult> Checkout(OrderViewModel OrderModel, [FromServices] IOrderService OrderService)
         {
             if (!ModelState.IsValid)
---
+++ WebStore_MVC/Controllers/UserProfileController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore_MVC.Services.Interfaces;
+using WebStore_MVC.ViewModels;
+
+namespace WebStore_MVC.Controllers
+{
+    [Authorize]
+    public class UserProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Orders([FromServices] IOrderService orderService)
+        {
+            var orders = await orderService.GetUserOrder(User.Identity!.Name);
+            return View(orders.Select(o => new UserOrderViewModel
+            {
+                Id = o.Id,
+                Name = o.Name,
+                Address = o.Address,
+                Phone = o.Phone,
+                TotalPrice = o.Items.Sum(item => item.TotalItemPrice)
+            }));
+        }
+    }
+}
