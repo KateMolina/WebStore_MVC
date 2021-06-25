b35b756d3210a231b2fcb14dd61c4c8a109b5284
KateM <molinakate888@gmail.com>
2021-06-25 18:50:59 -0500

    Interface_TestWebAPI

---
+++ Services/WebStore.Interfaces/TestAPI/IValuesInterface.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebStore.Interfaces.TestAPI
+{
+   public interface IValuesInterface
+    {
+        IEnumerable<string> GetAll();
+
+        string GetByIndex(int index);
+
+        void Add(int index);
+
+        void Edit(int index, string s);
+
+        void Delete(int index);
+
+    }
+}
