402424458f8d89a590454d6931bed3ffdd130b77
KateM <molinakate888@gmail.com>
2021-07-03 18:04:16 -0500

    User/RoleControllersWithTestMethodsAdded

---
+++ Services/WebStore.API/Controllers/Identity/RolesAPIController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.DAL.Context;
+using WebStore.Domain.Entities.Identity;
+using WebStore.Interfaces;
+
+namespace WebStore.API.Controllers.Identity
+{
+    [Route(WebAPIAddresses.Roles)]
+    [ApiController]
+    public class RolesAPIController : ControllerBase
+    {
+        private readonly RoleStore<Role> roleStore;
+
+        public RolesAPIController(WebStoreDB db)
+        {
+            roleStore = new RoleStore<Role>(db);
+        }
+
+        [HttpGet("all")]
+        public async Task<IEnumerable<Role>> GetAllRoles() => await roleStore.Roles.ToArrayAsync();
+
+    }
+}
---
+++ Services/WebStore.API/Controllers/Identity/UsersAPIController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.DAL.Context;
+using WebStore.Domain.Entities.Identity;
+using WebStore.Interfaces;
+
+namespace WebStore.API.Controllers.Identity
+{
+    [Route(WebAPIAddresses.Users)]
+    [ApiController]
+    public class UsersAPIController : ControllerBase
+    {
+        private readonly UserStore<User, Role, WebStoreDB> userStore;
+
+        public UsersAPIController(WebStoreDB db)
+        {
+            userStore = new UserStore<User, Role, WebStoreDB>(db);
+        }
+
+        [HttpGet("all")]
+        public async Task<IEnumerable<User>> GetAllUsers() => await userStore.Users.ToArrayAsync();
+    }
+}
