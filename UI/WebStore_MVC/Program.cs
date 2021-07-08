d2f29254ebbf495e73762b595b8a6e29e288848f
KateM <molinakate888@gmail.com>
2021-07-07 20:57:08 -0500

    SerilogSetup

--- UI/WebStore_MVC/Program.cs
+++ UI/WebStore_MVC/Program.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Formatting.Json;
 
 namespace WebStore_MVC
 {
@@ -15,14 +17,23 @@ namespace WebStore_MVC
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-            .ConfigureLogging((host, log) => log
-            .ClearProviders()
-            .AddDebug()
-            .AddConsole()
-            .AddEventLog())
-            
+                //.ConfigureLogging((host, log) => log
+                //.ClearProviders()
+                //.AddDebug()
+                //.AddConsole()
+                //.AddEventLog())            
                 .ConfigureWebHostDefaults(host => host
                 .UseStartup<Startup>()
-                );
+                )
+            .UseSerilog((host, log)=> log.ReadFrom.Configuration(host.Configuration)
+            .MinimumLevel.Debug()
+            .MinimumLevel.Override("Microsoft", Serilog.Events.LogEventLevel.Error)
+            .Enrich.FromLogContext()
+            .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss.fff} {Level:u3}]{SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}")
+                  .WriteTo.RollingFile($@".\Logs\WebStore[{DateTime.Now:yyyy-MM-ddTHH-mm-ss}].log")
+                  .WriteTo.File(new JsonFormatter(",", true), $@".\Logs\WebStore[{DateTime.Now:yyyy-MM-ddTHH-mm-ss}].log.json")
+                  //.WriteTo.Seq("http://localhost:5341")
+                  )
+            ;
     }
 }
