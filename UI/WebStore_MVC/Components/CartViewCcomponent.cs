f1c7522a85b98ce24e0349da7e21b63a21928051
KateM <molinakate888@gmail.com>
2021-08-08 15:18:20 -0500

    CreatingCartComponentLogicToSowCountInBracesAndAddingInsideUserInfoComponent

---
+++ UI/WebStore_MVC/Components/CartViewCcomponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore_MVC.Services.Interfaces;
+
+namespace WebStore_MVC.Components
+{
+    public class CartViewCcomponent : ViewComponent
+    {
+        private readonly ICartService cartService;
+
+        public CartViewCcomponent(ICartService cartService)
+        {
+            this.cartService = cartService;
+        }
+
+        public IViewComponentResult Invoke() => View(cartService.GetCartViewModel());
+    }
+}
