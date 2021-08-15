a70d377dd8417d01ce1f07b9e7381d16bd0b9be5
KateM <molinakate888@gmail.com>
2021-08-14 19:50:00 -0500

    JSForIncrementButton_Checkout

--- UI/WebStore_MVC/wwwroot/js/Cart/CartLogic.js
+++ UI/WebStore_MVC/wwwroot/js/Cart/CartLogic.js
@@ -9,7 +9,16 @@
     init: function (properties) {
         $.extend(Cart._properties, properties);
 
+        Cart.initEvents();
+    },
+
+    initEvents: function () {
         $(".add-to-cart").click(Cart.addToCart);
+
+        $(".cart_quantity_up").click(Cart.incrementItem);
+        $(".cart_quantity_down").click(Cart.decrementItem);
+        $(".cart_quantity_delete").click(Cart.removeItem);
+
     },
 
     addToCart: function (event) {
@@ -39,5 +48,72 @@
                 container.html(cartHtml);
             })
             .fail(function () { console.log("refreshCartView fail"); }); 
+    },
+
+    incrementItem: function (event) {
+        event.preventDefault();
+
+        var button = $(this);
+        const id = button.data("id");
+
+        var tr = button.closest("tr");
+
+        $.get(Cart.addToCartLink + "/" + id)
+            .done(function () {
+                const count = parseInt($(".cart_quantity_input", tr).val());
+                $(".cart_quantity_input", tr).val(count + 1);
+                Cart.refreshPrice(tr);
+                Cart.refreshCartView();
+
+            })
+            .fail(function () { console.log("incrementItem fail") });
+
+    },
+
+    refreshPrice: function (tr) {
+        const count = parseInt($(".cart_quantity_input", tr).val());
+        const price = parseInt($(".cart_price", tr).data("price"));
+
+        const totalPrice = price * count;
+        const value = totalPrice.toLocaleString("us-US", { style: "currency", currency: "$" });
+        const cartTotalPrice = $(".cart_total_price", tr);
+        cartTotalPrice.data("price", totalPrice);
+        cartTotalPrice.html(value);
+
+        Cart.refreshTotalPrice();
+    },
+
+    refreshTotalPrice: function () {
+        var totalPrice = 0;
+
+        $(".cart_total_price").each(function () {
+            const price = parseFloat($(this).data("price"));
+            totalPrice += price;
+        });
+        const value = totalPrice.toLocaleString("us-US", { style: "currency", currency: "$" });
+
+        $("#total-order-price").html(value);
+    },
+
+    decrementItem: function (event) {
+        event.preventDefault();
+
+        var button = $(this);
+        const id = button.data("id");
+
+        $.get(Cart.addToCartLink + "/" + id)
+            .done(function () { })
+            .fail(function () { console.log("decrementItem fail"));
+
+    },
+    removeItem: function (event) {
+        event.preventDefault();
+
+        var button = $(this);
+        const id = button.data("id");
+
+        $.get(Cart.addToCartLink + "/" + id)
+            .done(function () { })
+            .fail(function () { console.log("removeItem fail"));
     }
 }
