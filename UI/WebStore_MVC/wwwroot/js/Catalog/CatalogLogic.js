0c41b43a4d2151d6df53f877b235f699d5851541
KateM <molinakate888@gmail.com>
2021-08-22 22:57:33 -0500

    PrepScript_AsyncCatalogRefresh

---
+++ UI/WebStore_MVC/wwwroot/js/Catalog/CatalogLogic.js
@@ -0,0 +1,17 @@
+﻿Catalog = {
+    _properties: {
+        getViewLink: ""
+    },
+
+    init: properties => {
+        $.extend(Catalog._properties, properties);
+
+        $(".pagination li a").click(Catalog.clickOnPage);
+    },
+
+    clickOnPage: function (e) {
+        e.preventDefault();
+
+        const button = $(this);
+    }
+}
