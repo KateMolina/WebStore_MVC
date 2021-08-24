64f62033d957f0108ad5a45b15e608fa6f527aa9
KateM <molinakate888@gmail.com>
2021-08-24 13:31:24 -0500

    AjaxTestController_Prep

--- UI/WebStore_MVC/Controllers/AjaxTestController.cs
+++ UI/WebStore_MVC/Controllers/AjaxTestController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebStore_MVC.ViewModels;
 
 namespace WebStore_MVC.Controllers
 {
@@ -12,5 +13,30 @@ namespace WebStore_MVC.Controllers
         {
             return View();
         }
+
+        public async Task<IActionResult> GetJSON(int? id, string msg, int Delay = 2000)
+        {
+            await Task.Delay(Delay);
+
+            return Json(new
+            {
+                Message = $"Response(id:{id ?? 1}): {msg ?? "<null>"}",
+                ServerTime = DateTime.Now
+            });
+        }
+
+        public async Task<IActionResult> GetHTML(int? id, string msg, int Delay = 2000)
+        {
+            await Task.Delay(Delay);
+
+            return PartialView("Partial/_DataView", new AjaxTestDataViewModel
+            {
+                Id = id ?? 1,
+                Message = msg,
+            });
+        }
+
+
+
     }
 }
---
+++ UI/WebStore_MVC/ViewModels/AjaxTestDataViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore_MVC.ViewModels
+{
+    public class AjaxTestDataViewModel
+    {
+        public int Id { get; set; }
+        public string Message { get; set; }
+
+        public DateTime ServerTime { get; set; } = DateTime.Now;
+    }
+}
